b2d3c1316d147e5c413598b7e2b568b5783ec844
Damirka1 <hamitov.damir3101@icloud.com>
2022-10-25 16:27:26 +0600

    Initial commit

---
+++ src/main/java/com/dam1rka/TelegramBot/config/BotConfig.java
@@ -0,0 +1,17 @@
+package com.dam1rka.TelegramBot.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+public class BotConfig {
+
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${bot.token}")
+    String botToken;
+}
---
+++ src/main/java/com/dam1rka/TelegramBot/config/BotStart.java
@@ -0,0 +1,34 @@
+package com.dam1rka.TelegramBot.config;
+
+import com.dam1rka.TelegramBot.services.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Component
+public class BotStart {
+    private final TelegramBot telegramBot;
+
+    public BotStart(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @PostConstruct
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(telegramBot);
+        }
+        catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
+}
